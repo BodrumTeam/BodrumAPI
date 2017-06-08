c530c2a42b3608deb2fbecbcf5bd45a89dde4d29
Hassona75 <moh.hassona244@gmail.com>
2017-06-08 12:28:03 +0200

    commit

--- API/App_Start/WebApiConfig.cs
+++ API/App_Start/WebApiConfig.cs
@@ -9,7 +9,7 @@ namespace API
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
-        {
+        { 
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
