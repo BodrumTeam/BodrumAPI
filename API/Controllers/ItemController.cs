f96d04f6260cd779666636e5554d28b317e7f6f0
Hassona75 <moh.hassona244@gmail.com>
2017-06-12 10:23:43 +0200

    checkin

---
+++ API/Controllers/ItemController.cs
@@ -0,0 +1,43 @@
+﻿using API.BLL.UnitOFWork;
+using Entity.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    [RoutePrefix("rpc/item")]
+    public class ItemController : ApiController
+    {
+        private UnitOfWork uow = new UnitOfWork();
+
+        [Route("getAll")]
+        public IEnumerable<Item> Get()
+        {
+            return uow.ItemManager.GetAll().ToList();
+        }
+        
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Item
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Item/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Item/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
